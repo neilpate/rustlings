0fde6acc47feacd598483e2a8610f31e44c0adf2
Neil <neil@premiergsolutions.com>
2022-08-26 20:11:29 +0200

    And more

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -1,13 +1,25 @@
 // clippy2.rs
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     let mut res = 42;
+   
+   
     let option = Some(12);
-    for x in option {
+   
+   
+    if let Some(x) = option {
         res += x;
-    }
+    } 
+    
+    // if let Some(y) = 42 {
+    //     for x in y {
+    //         res += x;
+    //     }
+     
+        
+    // } 
+   
     println!("{}", res);
 }
